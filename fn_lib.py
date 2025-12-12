3a8ecbc6b8d1b843974c2af69a7fc80485f6e411
Akshay Sanjeev <asanjeevkeloth@gmail.com>
2025-12-12 11:52:55 +0530

    16 cities and kPDF

--- fn_lib.py
+++ fn_lib.py
@@ -6,19 +6,19 @@ import osmnx as osm
 import os
 
 def ExtractGraph(City, code):
-    G = osm.graph_from_place(f"{city_name}, India", network_type="drive")
+    G = osm.graph_from_place(f"{City}, India", network_type="drive")
 
-    with open(f"./graph_data/{city_code}.gpickle", "wb") as f:
+    with open(f"./graph_data/{code}.gpickle", "wb") as f:
         pickle.dump(G, f)
-    V,E = nx.number_of_nodes(G), nx.number_of_edges(G)
-    print(f"V = {V} \n E= {E}")
+    # V,E = nx.number_of_nodes(G), nx.number_of_edges(G)
+    # print(f"V = {V} \n E= {E}")
     return G 
 
 def OpenGraph(citycode):
     with open(f"./graph_data/{citycode}.gpickle", "rb") as file:
         G = pickle.load(file)
-    V,E = nx.number_of_nodes(G), nx.number_of_edges(G)
-    print(f"\n #Vertices = {V} \n #Edges = {E}")
+    # V,E = nx.number_of_nodes(G), nx.number_of_edges(G)
+    # print(f"\n #Vertices = {V} \n #Edges = {E}")
     return G
 
 def kPDF(G, city, binsize=1, save=False):
@@ -33,10 +33,13 @@ def kPDF(G, city, binsize=1, save=False):
     # ax.set_xscale('log')
     ax.set_yscale('log')
     ax.plot(edges[1:],pk, '-o',
-            color='black', fillstyle='none')
+            color='black', fillstyle='none',
+            linewidth=1)
     ax.set_title(f"{city}")
     ax.set_xlabel("$k$")
     ax.set_ylabel("$p(k)$")
+    ax.set_ylim(0, 1)
+    ax.set_xlim(0, 12)
     if save == True:
         fig.savefig(f"./kPDF/{city}.png", dpi=450)
     else:
