bb1e6417f1a4f25977df8d3409e6a25da2d70a6b
Akshay Sanjeev <asanjeevkeloth@gmail.com>
2025-12-12 10:43:40 +0530

    nill

---
+++ fn_lib.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import networkx as nx
+import osmnx as osm
+import os
+
+def ExtractGraph(City, code):
+    G = osm.graph_from_place(f"{city_name}, India", network_type="drive")
+
+    with open(f"./graph_data/{city_code}.gpickle", "wb") as f:
+        pickle.dump(G, f)
+    V,E = nx.number_of_nodes(G), nx.number_of_edges(G)
+    print(f"V = {V} \n E= {E}")
+    return G 
+
+def OpenGraph(citycode):
+    with open(f"./graph_data/{citycode}.gpickle", "rb") as file:
+        G = pickle.load(file)
+    V,E = nx.number_of_nodes(G), nx.number_of_edges(G)
+    print(f"\n #Vertices = {V} \n #Edges = {E}")
+    return G
+
+def kPDF(G, city, binsize=1, save=False):
+    d = np.array([deg for _, deg in G.degree()])
+    dmin, dmax = d.min(), d.max()
+    binsize=1
+    bins = np.arange(dmin, dmax+binsize, binsize)
+    counts, edges = np.histogram(d, bins=bins)
+    pk = counts/counts.sum()
+    
+    fig, ax = plt.subplots(figsize=(5, 4))
+    # ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.plot(edges[1:],pk, '-o',
+            color='black', fillstyle='none')
+    ax.set_title(f"{city}")
+    ax.set_xlabel("$k$")
+    ax.set_ylabel("$p(k)$")
+    if save == True:
+        fig.savefig(f"./kPDF/{city}.png", dpi=450)
+    else:
+        None
+    return fig
