8a7aaaaa030aef1ffffc121541a4deccb0d63cf2
askay-sanjeev <asanjeevkeloth@gmail.com>
2025-12-15 15:46:48 +0530

    nill

--- run_unWRW.py
+++ run_unWRW.py
@@ -25,9 +25,39 @@ def unW_RW(G0, n, tmax):
         x[:, t] = (Tcdf >= r[:, None]).argmax(axis=1) #ITS updation
     return x
 
-def unWRWplot(G0, n, tmax):
-    X = unW_RW(G0, n, tmax)
-    N = A0.shape[0]
+
+def unW_RW_EL(G, n, tmax):
+    # Ensure nodes are indexed 0..N-1
+    G = nx.convert_node_labels_to_integers(G)
+    
+    N = G.number_of_nodes()
+    
+    # Build adjacency list
+    neighbors = {i: list(G.neighbors(i)) for i in range(N)}
+
+    x = np.zeros((n, tmax), dtype=int)
+    x[:, 0] = np.random.choice(N, n)
+
+    for t in tqdm(range(1, tmax)):
+        xt = x[:, t-1]
+
+        for i in range(n):
+            node = xt[i]
+            nbrs = neighbors[node]
+
+            if len(nbrs) == 0:
+                x[i, t] = node
+            else:
+                x[i, t] = np.random.choice(nbrs)
+
+    return x
+
+def unWRWplot(n, tmax, source="graph",G0=None, filename=None):
+    if source=="graph":
+        X = unW_RW(G0, n, tmax)
+    elif source == "file":
+        X = np.load(f"./{filename}")['arr']
+    N = np.unique(X)
     n = X.shape[0]
 
     P = np.zeros((N, tmax), dtype=float)
@@ -44,9 +74,10 @@ def unWRWplot(G0, n, tmax):
 
 
 
+
 if __name__ == "__main__":
-    city = "test"
+    city = "CHE"
     G0 = OpenGraph(city)    
     n = int(1e3); tmax = int(1e3)
-    X = unW_RW(G0, n, tmax)
+    X = unW_RW_EL(G0, n, tmax)
     np.savez_compressed(f"./RW_{city}_n{n}_t{tmax}.npz", arr=X)
